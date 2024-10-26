fb2f5c7f73ae6bc1c65184da122d2961035cbc54
MChance176 <matt.chance176@gmail.com>
2024-10-26 17:07:16 -0400

    Updated generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,13 +10,29 @@ function renderLicenseLink(license) {
   return `https://opensource.org/licenses/${encodeURIComponent(license)}`;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render license section
+function renderLicenseSection(license) {
+  if (!license) return "";
 
-// TODO: Create a function to generate markdown for README
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
+  return `## License
+
+  This project is licensed under the ${license} license.
+
+${badge}
+
+For more information, see the [LICENSE](${link}) file.`;
+}
+
+// Function to generate markdown
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseSection(data.license)}
+
+##
 
 `;
 }
