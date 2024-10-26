4a8ee0a42d5401b6d275399430f600da0cd6dadf
MChance176 <matt.chance176@gmail.com>
2024-10-26 17:54:41 -0400

    added updated generateMarkdown.js

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-// TODO: Include packages needed for this application
+// Import required modules: inquirer for user prompts, fs for file operations, and custom generateMarkdown function
+import inquirer from 'inquirer';
+import fs from 'fs/promises';
+import generateMarkdown from './utils/generateMarkdown.js';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of the project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email:',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
